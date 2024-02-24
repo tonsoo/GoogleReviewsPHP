48d1d3e00bc920215fdca44dff9bf9ed9514ee05
tons0 <alyssuuuuu@outlook.com>
2024-02-24 00:05:01 -0300

    Update GoogleReviews.php

--- GoogleReviews.php
+++ GoogleReviews.php
@@ -1,6 +1,11 @@
 <?php
 
 class GoogleReviews{
+    /*
+
+    Declaração das constantes utilizadas para exibição das reviews completas ou simplificadas.
+
+    */
     const FULL_REVIEW = 0;
     const SIMPLE_REVIEW = 1;
 
@@ -14,6 +19,13 @@ class GoogleReviews{
 
         $this->fetchReviews();
     }
+
+    /*
+
+    Função principal responsavel pela requisição ao endereço https://places.googleapis.com/v1/places/
+    para recuperar as reviews do Google com base na chave da API e id do local
+
+    */
 
     private function fetchReviews(int $reviewLevel=GoogleReviews::SIMPLE_REVIEW) : void{
         $query = http_build_query(['key' => $this->apiKey, 'fields' => 'reviews']);
@@ -31,6 +43,12 @@ class GoogleReviews{
         if(empty($data) || isset($data['error']) || !isset($data['reviews'])){
             return;
         }
+
+        /*
+
+        Modificação do retorno das reviews com base no tipo de reviews escolhida na criação do objeto
+
+        */
 
         switch($reviewLevel){
             case GoogleReviews::SIMPLE_REVIEW:
@@ -69,8 +87,17 @@ class GoogleReviews{
     }
 }
 
+// Criação do objeto passando os parametros obrigatorios 'apiKey' e 'locationId', junto do parametro facultativo 'reviewLevel'
 $reviews = new GoogleReviews('id_da_api_do_google_aqui', 'id_do_local_do_google_aqui', GoogleReviews::SIMPLE_REVIEW);
 
-$reviews->getReviews();
+// Recupera o array das reviews
+$arrayDasReviews = $reviews->getReviews();
 
+/*
+
+Codigo com o 'arrayDasReviews'...
+
+*/
+
+// Renderização das reviews com uma arrow function
 $reviews->iterate(fn($review) => print("<div>{$review['author']}</div>"));
